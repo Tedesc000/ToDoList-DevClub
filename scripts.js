daba7ae6487022d8c9674dfefb0cb72c0c2e4c5d
Eduardo Rosa Tedesco <eduardo.tedesco19@gmail.com>
2026-02-22 12:11:39 -0300

    Alterei a lógica de como são montadas as tarefas para permitir checkbox.

--- scripts.js
+++ scripts.js
@@ -2,26 +2,33 @@
 let contador = 1;
 
 function adicionarTarefa(){
-    let inputTarefa = document.getElementById("inputTarefa").value;
-    if(inputTarefa === ""){
+    let inputTarefa = document.getElementById("inputTarefa");
+    let textoTarefa = inputTarefa.value.trim();
+
+    if(textoTarefa === ""){
         alert("Por favor, insira uma tarefa.");
-    }else{
-        let lista = document.getElementById("lista").innerHTML;
-        lista += `
+        return;
+    }
+
+
+        let lista = document.getElementById("lista");
+        //Antes utilizava innerHTML +=, mas isso causava problemas de desempenho e perda de eventos como checkbox que foram clicados. Agora, utilizo insertAdjacentHTML para adicionar o novo item sem re-renderizar toda a lista.
+        lista.insertAdjacentHTML("beforeend", `
             <li class="itemLista" id="tarefa${contador}">
             <div class="conteudoTarefa">
-                <div class="numeroTarefa">${contador}</div>. ${inputTarefa}
+            <input type="checkbox" class="checkbox" id="checkbox${contador}">
+                <label for="checkbox${contador}" class="labelTarefa">
+                    <div class="numeroTarefa">${contador}</div>. ${textoTarefa}
+                </label>
             </div>
                 <button class="concluir" onclick="fecharTarefa(${contador})">
                     <i class="bi bi-x-circle fechar"></i>
                 </button>
             </li>
-            `;
-        document.getElementById("lista").innerHTML = lista;
-        document.getElementById("inputTarefa").value = "";
+            `);
+        inputTarefa.value = "";
         contador++;
     }
-}
 
 function fecharTarefa(tarefaId){
     document.getElementById(`tarefa${tarefaId}`).remove();
